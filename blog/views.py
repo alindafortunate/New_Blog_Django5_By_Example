fd371804672b3e8ad2cab99f00b017d6c55f6069
ALINDA Fortunate <alindafortunate5@gmail.com>
2024-10-30 18:48:23 +0300

    Implemented pagination

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, Paginator
 from django.shortcuts import render
 from .models import Post
 from django.http import Http404
@@ -6,15 +7,31 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)  # Paginator with three posts per page.
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(
+            paginator.num_pages
+        )  # If page_number is out of range get last page of results
+
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No post found.")
     # return render(request, "blog/post/detail.html", {"post": post})
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, "blog/post/detail.html", {"post": post})
