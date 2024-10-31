380a4722715e070d034d6c8055d22ebf515a2c35
ALINDA Fortunate <alindafortunate5@gmail.com>
2024-10-31 17:23:07 +0300

    Implemented mail service by Django

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,32 @@
-from django.core.paginator import EmptyPage, Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from .forms import EmailPostForm
 
 
 # Create your views here.
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)  # Paginator with three posts per page.
     page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)  # If page is not an integer, get the first page.
     except EmptyPage:
         posts = paginator.page(
             paginator.num_pages
@@ -21,11 +36,7 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("No post found.")
-    # return render(request, "blog/post/detail.html", {"post": post})
+
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
@@ -35,3 +46,18 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == "POST":
+        # Form was submitted.
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        # Form fields passed validation.
+        cd = form.cleaned_data
+    # send email
+    else:
+        form = EmailPostForm()
+    return render(request, "blog/post/share.html", {"post": post, "form": form})
--- myblog/settings.py
+++ myblog/settings.py
@@ -15,6 +15,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+# Another way to work with environment variables.
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +132,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email Server configurations
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
