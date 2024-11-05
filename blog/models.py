24ccbdd7f13ecb3bec03928197071c62cb493995
ALINDA Fortunate <alindafortunate5@gmail.com>
2024-11-05 22:37:46 +0300

    styled the comment_form.html

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 # Create your models here.
@@ -27,6 +28,8 @@ class Post(models.Model):
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # The custom manager.
+
+    tags = TaggableManager()
 
     class Meta:
         ordering = [
--- myblog/settings.py
+++ myblog/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "taggit",
     # Local apps
     "blog.apps.BlogConfig",
 ]
