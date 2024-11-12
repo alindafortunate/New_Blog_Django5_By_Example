439fb6ace6a052c10eb51e4d01f46c3245a25dec
ALINDA Fortunate <alindafortunate5@gmail.com>
2024-11-12 16:31:46 +0300

    Implemented sitemap

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,11 +1,13 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Post
 
+
 class PostSiteMap(Sitemap):
-    changefreq='weekly'
-    priority=0.9
+    changefreq = "weekly"
+    priority = 0.9
 
     def items(self):
         return Post.published.all()
+
     def lastmod(self, obj):
-        return obj.pub__date
+        return obj.updated
